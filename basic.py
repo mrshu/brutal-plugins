140d2217edf9a7f337812645539b1f961a0b54f1
mr.Shu <mr@shu.io>
2014-10-26 23:37:58 +0100

    update: Added a few comments
    * Added a few comments as docstrings for @cmd functions.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- basic.py
+++ basic.py
@@ -4,11 +4,14 @@ from brutal.core.plugin import BotPlugin, cmd, event, match, threaded
 
 @cmd
 def ping(event):
+    """Responds 'pong' to your 'ping'."""
+
     return 'pong'
 
 
 @cmd
 def echo(event):
+    """Echoes back the message it recieves."""
     return ' '.join(event.args)
 
 
