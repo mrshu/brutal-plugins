2fac2cd85e19885e9e6a42fe411aa8f2ad2a6c95
Jakub Novak <jakub.novak@gmail.com>
2014-11-20 20:28:33 +0100

    Countdown: Added min. delay time to prevent spam.
     * Return messages are now in variables.
     * Added check for empty task message.
     * Renamed method and variables to more fitting names.
     * Changed output message after counter is done.
     * Added some comments.
    
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- countdown.py
+++ countdown.py
@@ -2,40 +2,61 @@
 from brutal.core.plugin import BotPlugin, cmd
 
 class Countdown(BotPlugin):
-    minutes = 0
-    #in seconds
+    counter = 0
+    # countdown_delay represented in seconds
     countdown_delay = 0
     text = ""
 
 
     @cmd
     def countdown(self, event):
-        """Creates timer and returns reminder after it expires."""
-        
-        if len(event.args) < 2:
-            return "Not a valid countdown request, try again!"
+        """Creates countdowner that will count down from a specified value down 
+           to 0 adn then reminds you the time is up."""
 
-        if not (event.args[0]).isdigit():
-            return "Not a valid countdown request, try again!"
+        BAD_REQ = "Not a valid countdown request, try again!"
+        LD_MSG = "Requested delay is too low. Setting lowest possible(30)."
+
+        if len(event.args) < 2:
+            return BAD_REQ
+
+        if not event.args[0].isdigit():
+            return BAD_REQ
 
         second_arg = event.args[1]
-        self.minutes = int(event.args[0]) 
-        if(second_arg.isdigit()):
-            self.countdown_delay = int(second_arg)
-            self.text = ' '.join(event.args[2:])
+        self.counter = int(event.args[0]) * 60
+
+        if second_arg.isdigit():
+            task_arg = event.args[2:]
+            if not task_arg:
+                return BAD_REQ
+            else:
+                # Setting delay to at least 30seconds prevents too much spam.
+                if int(second_arg) < 30:
+                    self.msg(LD_MSG, event=event)
+                    self.countdown_delay = 30
+                else:
+                    self.countdown_delay = int(second_arg)
+
+                self.text = ' '.join(task_arg)
         else:
-            self.countdown_delay = 60
-            self.text = ' '.join(event.args[1:])
+            task_arg = event.args[1:]
+            if not task_arg:
+                return BAD_REQ
+            else:
+                self.countdown_delay = 60
+                self.text = ' '.join(task_arg)
     
-        self.delay_task(self.countdown_delay, self.dec, event=event)
+        self.delay_task(self.countdown_delay, self.decrementer, event=event)
         
         return "Starting coundtown for {0}".format(self.text)
 
 
-    def dec(self, event):
-        self.minutes -= 1
-        if self.minutes != 0:
-            self.msg("T-{0} ( {1} )".format(self.minutes, self.text), event=event)
-            self.delay_task(self.countdown_delay, self.dec, event=event)
+    def decrementer(self, event):
+        self.counter -= self.countdown_delay
+        if self.counter != 0:
+                self.msg("T-{0}sec ( {1} )".format(self.counter, self.text), 
+                     event=event)
+                self.delay_task(self.countdown_delay, self.decrementer, 
+                            event=event)
         else:
-            self.msg("It's happening! {0}".format(self.text), event=event)    
+            self.msg("{0} is happening!".format(self.text), event=event)    
