09ee37664a55ac48f64af7139e918dda394f5332
mr.Shu <mr@shu.io>
2014-11-24 23:02:09 +0100

    update: Fixed bugs in eval
    * Fixed the `abs` function.
    * Fixed `eval` being called without arguments.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- basic.py
+++ basic.py
@@ -47,14 +47,17 @@ def auto_welcome(event):
 
 # math functions need to be imported at runtime
 from math import *
-safe_list = ['acos', 'abs', 'asin', 'atan', 'atan2', 'ceil', 'cos', 'cosh',
+safe_list = ['acos', 'asin', 'atan', 'atan2', 'ceil', 'cos', 'cosh',
              'degrees', 'e', 'exp', 'fabs', 'floor', 'fmod', 'frexp', 'hypot',
              'ldexp', 'log', 'log10', 'modf', 'pi', 'pow', 'radians', 'sin',
              'sinh', 'sqrt', 'tan', 'tanh']
 safe_dict = dict([(k, locals().get(k, None)) for k in safe_list])
 
+# abs is a global builtin function and so it is not in locals()
+safe_dict['abs'] = abs
 
-@cmd(command='^eval')
+
+@cmd(command='eval')
 def eval_(event):
     # the name eval_ is used in order to avoid clashing with the built-in eval
     # function which is actually how the eval command is implemented.
@@ -72,4 +75,7 @@ def eval_(event):
     """
 
     input = ' '.join(event.args)
+    if len(input) < 1:
+        return
+
     return eval(input, {"__builtins__": None}, safe_dict)
