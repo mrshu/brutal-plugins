3c49718677cd3fd37602d84d61c9ccb37903682c
Adrian Matejov <a.matejov@centrum.sk>
2014-11-30 11:34:17 +0100

    travel: cpsk plugin
    * searches for bus/train lines in Slovakia
    * ability to match query or use cmds !bus or !vlak
    * time and date specification feature
    
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- travel.py
+++ travel.py
@@ -1,6 +1,9 @@
 import imhdsk
+import cpsk
 from brutal.core.plugin import match, cmd, threaded
 import codecs
+import datetime
+import re
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -81,3 +84,90 @@ def mhd(event):
     out = r[0].__repr__()
     out = unicode(out.strip(codecs.BOM_UTF8), 'utf-8')
     return out.encode('utf-8')
+
+
+@threaded
+@match(regex=r'(?:.*\s+|)(?:bus|vlak|spoj)\sz\s([A-Za-z\s]+)\sdo\s([A-Za-z\s]+)(?:.*\s+|)')
+def cpsk_match(event, departure, dest, *args):
+    """Searches for bus or train info in Slovakia.
+
+    Examples:
+        bus z BA do TO
+        najblizsi vlak z Topolcany do Prievidza
+        spoj z KE do Trencin
+        zajtra bus z TO do Jacovce
+        pozajtra o 20:30 bus z TO do BA
+    """
+    msg = event.meta['body']
+
+    date = ''
+    if 'zajtra' in msg:
+        date = (datetime.date.today() + datetime.timedelta(days=1)) \
+                    .strftime("%d.%m.%Y")
+    elif 'pozajtra' in msg:
+        date = (datetime.date.today() + datetime.timedelta(days=2)) \
+                    .strftime("%d.%m.%Y")
+
+    time_match = re.search("([0-9]+:[0-9]+)", msg)
+    time = ''
+    if time_match is not None:
+        time = msg[time_match.start():time_match.end()]
+
+    vehicle = 'vlakbus'
+    if 'vlak' in msg:
+        vehicle = 'vlak'
+    elif 'bus' in msg:
+        vehicle = 'bus'
+
+    routes = cpsk.get_routes(departure, dest, vehicle=vehicle,
+                                 time=time, date=date)
+
+    return routes[0].__repr__()
+
+
+def get_line(event, vehicle):
+    """Searches for bus/train based on given vehicle argument"""
+    args = event.args
+
+    if len(args) < 2:
+        return
+
+    if '-' in args:
+        a = ' '.join(args)
+        args = map(lambda x: x.strip(), a.split('-'))
+
+    dep = args[0]
+    dest = args[1]
+
+    time = args[2] if len(args) > 2 else ''
+    date = args[3] if len(args) > 3 else ''
+
+    if dep == dest:
+        return "You joker"
+
+    r = cpsk.get_routes(dep, dest, vehicle=vehicle, time=time, date=date)
+    return r[0].__repr__() if len(r) else "Nothing found"
+
+
+@threaded
+@cmd
+def bus(event):
+    """Command for bus lines.
+
+    Examples:
+        !bus BA TO
+    """
+
+    return get_line(event, 'bus')
+
+
+@threaded
+@cmd
+def vlak(event):
+    """Command for train lines.
+
+    Examples:
+        !vlak Kosice Bratislava
+
+    """
+    return get_line(event, 'vlak')
