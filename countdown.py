8418c73ba9903613ae57720eebedc6bacfdadb2d
Jakub Novak <jakub.novak@gmail.com>
2014-12-01 22:05:46 +0100

    Countdown: Reworked countdown.
     * Removed the option to set delay time.
     * Countdown now automatically changes delay time depending
       on time left.
     * Fixed a typo in docstring.
    
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- countdown.py
+++ countdown.py
@@ -6,57 +6,75 @@ LOW_DELAY_MSG = "Requested delay is too low. Setting lowest possible(30)."
 
 class Countdown(BotPlugin):
     counter = 0
-    # countdown_delay represented in seconds
-    countdown_delay = 0
+    delay_above_ten = 300
+    delay_min = 60
+    delay_sec = 10
+    delay_last_ten = 1
     text = ""
-
 
     @cmd
     def countdown(self, event):
-        """Creates countdowner that will count down from a specified value down 
-           to 0 adn then reminds you the time is up."""
+        """Creates countdowner that will count down from a specified value down
+           to 0 and then reminds you the time is up."""
 
         if len(event.args) < 2:
             return BAD_REQ
-
         if not event.args[0].isdigit():
             return BAD_REQ
-
-        second_arg = event.args[1]
-        self.counter = int(event.args[0]) * 60
-
-        if second_arg.isdigit():
-            task_arg = event.args[2:]
-            if not task_arg:
-                return BAD_REQ
-            else:
-                # Setting delay to at least 30 seconds prevents too much spam.
-                if int(second_arg) < 30:
-                    self.msg(LOW_DELAY_MSG, event=event)
-                    self.countdown_delay = 30
-                else:
-                    self.countdown_delay = int(second_arg)
-
-                self.text = ' '.join(task_arg)
+        # countdown time
+        if int(event.args[0]) <= 1:
+            return BAD_REQ
         else:
-            task_arg = event.args[1:]
-            if not task_arg:
-                return BAD_REQ
-            else:
-                self.countdown_delay = 60
-                self.text = ' '.join(task_arg)
-    
-        self.delay_task(self.countdown_delay, self.decrementer, event=event)
-        
-        return "Starting coundtown for {0}".format(self.text)
+            self.counter = int(event.args[0]) * 60
+        # countdown text
+        task_arg = event.args[1:]
+        if not task_arg:
+            return BAD_REQ
+        else:
+            self.text = ' '.join(task_arg)
+
+        if self.counter > self.delay_above_ten:
+            self.delay_task(self.delay_above_ten, self.decrementer, event=event)
+        elif self.counter > self.delay_min:
+            self.delay_task(self.delay_min, self.decrementer, event=event)
+        elif self.counter == self.delay_min:
+            self.delay_task(self.delay_sec, self.decrementer, event=event)
+
+        return "Starting {0}min coundtown for {1}".format(self.counter // 60,
+                                                          self.text)
 
 
     def decrementer(self, event):
-        self.counter -= self.countdown_delay
+        if self.counter > self.delay_above_ten:
+            self.counter -= self.delay_above_ten
+        elif self.delay_above_ten >= self.counter > self.delay_min:
+            self.counter -= self.delay_min
+        elif self.delay_min >= self.counter > self.delay_sec:
+            self.counter -= self.delay_sec
+        elif self.delay_sec >= self.counter:
+            self.counter -= self.delay_last_ten
+
         if self.counter != 0:
-                self.msg("T-{0}sec ( {1} )".format(self.counter, self.text), 
-                     event=event)
-                self.delay_task(self.countdown_delay, self.decrementer, 
-                            event=event)
+            if self.counter > self.delay_above_ten:
+                self.msg("T-{0}min ({1})".format(self.counter // 60,
+                                                       self.text), event=event)
+                self.delay_task(self.delay_above_ten, self.decrementer, 
+                                event=event)
+
+            elif self.delay_above_ten >= self.counter > self.delay_min:
+                self.msg("T-{0}min ({1})".format(self.counter // 60,
+                                                       self.text), event=event)
+                self.delay_task(self.delay_min, self.decrementer, event=event)
+
+            elif self.delay_min >= self.counter > self.delay_sec:
+                self.msg("T-{0}sec ({1})".format(self.counter, self.text),
+                         event=event)
+                self.delay_task(self.delay_sec, self.decrementer, event=event)
+
+            elif self.delay_sec >= self.counter:
+                self.msg("T-{0}sec ({1})".format(self.counter, self.text),
+                         event=event)
+                self.delay_task(self.delay_last_ten, self.decrementer, 
+                                event=event)
         else:
-            self.msg("{0} is happening!".format(self.text), event=event)    
+            self.msg("{0} is happening!".format(self.text), event=event)
