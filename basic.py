073a0dbdc9840be3435fc0010c5c567500484af2
Jakub Novak <jakub.novak@gmail.com>
2014-12-17 12:50:41 +0100

    Basic: Added channel specific welcomes.
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- basic.py
+++ basic.py
@@ -26,23 +26,26 @@ def make(event):
     return 'what? make it yourself.'
 
 
-welcomes = {
-    "LordPotato_":
-        "Everybody on your knees and let's praise the mighty Potato!",
-    "pepol": "Nazimod sighted, take cover!",
-    "mrshu": "Hide yo codes, hide yo wife, nazireviewer is on site!",
-    "JakubN": "Swiggidy swooty, he's comin' for dat booty!",
-    "kalerab": "Hide your apples, 'cause he's gonna eat 'em!"
+welcomes_db = {
+    '#databazy': {
+        "LordPotato_":
+            "Everybody on your knees and let's praise the mighty Potato!",
+        "pepol": "Nazimod sighted, take cover!",
+        "mrshu": "Hide yo codes, hide yo wife, nazireviewer is on site!",
+        "JakubN": "Swiggidy swooty, he's comin' for dat booty!",
+        "kalerab": "Hide your apples, 'cause he's gonna eat 'em!"
+    }
 }
 
 
 @event
 def auto_welcome(event):
     if event.event_type == 'join':
-        if event.meta['nick'] in welcomes:
-            return welcomes[event.meta['nick']]
-        else:
-            return event.meta['nick'] + ': hi!'
+        if event.source_room in welcomes_db:
+            welcomes = welcomes_db[event.source_room]
+            if event.meta['nick'] in welcomes:
+                return welcomes[event.meta['nick']]
+        return event.meta['nick'] + ': hi!'
 
 
 # math functions need to be imported at runtime
