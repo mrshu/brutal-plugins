0ab023cf11cc0245539583866642636f89cab806
Jakub Novak <jakub.novak@gmail.com>
2014-12-17 12:50:41 +0100

    Basic: Added channel specific welcomes.
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>
    
    Basic: Temporary fix for name matching problem.
    
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>
    
    Basic: Added comment.
    
     * Adding .lower() fixes problem when a user e.g. "FeroT" logs in with a new nickname "feroT".
    
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>
    
    Basic: Fixed a comment.
    
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- basic.py
+++ basic.py
@@ -26,23 +26,28 @@ def make(event):
     return 'what? make it yourself.'
 
 
-welcomes = {
-    "LordPotato_":
-        "Everybody on your knees and let's praise the mighty Potato!",
-    "pepol": "Nazimod sighted, take cover!",
-    "mrshu": "Hide yo codes, hide yo wife, nazireviewer is on site!",
-    "JakubN": "Swiggidy swooty, he's comin' for dat booty!",
-    "kalerab": "Hide your apples, 'cause he's gonna eat 'em!"
+welcomes_db = {
+    '#databazy': {
+        "lordpotato_":
+            "Everybody on your knees and let's praise the mighty Potato!",
+        "pepol": "Nazimod sighted, take cover!",
+        "mrshu": "Hide yo codes, hide yo wife, nazireviewer is on site!",
+        "jakubn": "Swiggidy swooty, he's comin' for dat booty!",
+        "kalerab": "Hide your apples, 'cause he's gonna eat 'em!"
+    }
 }
 
 
 @event
 def auto_welcome(event):
     if event.event_type == 'join':
-        if event.meta['nick'] in welcomes:
-            return welcomes[event.meta['nick']]
-        else:
-            return event.meta['nick'] + ': hi!'
+        if event.source_room in welcomes_db:
+            welcomes = welcomes_db[event.source_room]
+            # .lower() is a temporary fix for differences between lowercase 
+            # and uppercase name maching
+            if event.meta['nick'].lower() in welcomes:
+                return welcomes[event.meta['nick']]
+        return event.meta['nick'] + ': hi!'
 
 
 # math functions need to be imported at runtime
