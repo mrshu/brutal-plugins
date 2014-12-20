bacbd9c6ad0d892c4e44adab7d261a7239440888
mr.Shu <mr@shu.io>
2014-12-20 17:01:57 +0100

    update: Refactored regex and cpsk matcher
    * Refactored a long regex into a shorter constant.
    
    * Removed overindent from cpsk matcher.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- travel.py
+++ travel.py
@@ -83,8 +83,12 @@ def mhd(event):
     return out.encode('utf-8')
 
 
+CPSK_REGEX = r'(?:.*\s+|)(?:bus|vlak|spoj)\sz\s([A-Za-z\s]+)' \
+    '\sdo\s([A-Za-z\s]+)(?:.*\s+|)'
+
+
 @threaded
-@match(regex=r'(?:.*\s+|)(?:bus|vlak|spoj)\sz\s([A-Za-z\s]+)\sdo\s([A-Za-z\s]+)(?:.*\s+|)')
+@match(regex=CPSK_REGEX)
 def cpsk_match(event, departure, dest, *args):
     """Searches for bus or train info in Slovakia.
 
@@ -100,10 +104,10 @@ def cpsk_match(event, departure, dest, *args):
     date = ''
     if 'zajtra' in msg:
         date = (datetime.date.today() + datetime.timedelta(days=1)) \
-                    .strftime("%d.%m.%Y")
+            .strftime("%d.%m.%Y")
     elif 'pozajtra' in msg:
         date = (datetime.date.today() + datetime.timedelta(days=2)) \
-                    .strftime("%d.%m.%Y")
+            .strftime("%d.%m.%Y")
 
     time_match = re.search("([0-9]+:[0-9]+)", msg)
     time = ''
@@ -117,7 +121,7 @@ def cpsk_match(event, departure, dest, *args):
         vehicle = 'bus'
 
     routes = cpsk.get_routes(departure, dest, vehicle=vehicle,
-                                 time=time, date=date)
+                             time=time, date=date)
 
     return routes[0].__repr__()
 
