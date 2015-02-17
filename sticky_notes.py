70726e8ee56c3d1186709293c0cb0e2ff5edb131
mr.Shu <mr@shu.io>
2015-02-17 23:59:23 +0100

    update: Created sticky notes
    * Created a way of preparing sticky notes for a given user which will be
      displayed to them once they interact with the bot in some way.
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ sticky_notes.py
@@ -0,0 +1,26 @@
+from brutal.core.plugin import BotPlugin, cmd, match
+
+
+class StickyNotes(BotPlugin):
+    def setup(self, *args, **kwargs):
+        self.notes = self.open_storage('sticky_notes')
+
+    @cmd
+    def stickynote(self, event):
+        """Create a sticky note for a given user."""
+        args = event.args
+
+        if len(args) < 1:
+            return "Sticky note needs to be adressed to someone."
+        elif len(args) < 2:
+            return "Sticky note needs to contain some actual note."
+        else:
+            user = event.args[0]
+            content = ' '.join(event.args[1:])
+
+        if user not in self.notes:
+            self.notes[user] = []
+
+        self.notes[user].append("Note from {0}: {1}".format(user, content))
+
+        return "Sticky note for {0} prepared.".format(user)
