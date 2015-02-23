6452ae545fe3957ac7f32451aaea0b55d4859275
Jakub Novak <jakub.novak@gmail.com>
2015-02-23 21:06:52 +0100

    sn: Added command to list notes for a given nick.
    Signed-off-by: Jakub Novak <jakub.novak@gmail.com>

--- sticky_notes.py
+++ sticky_notes.py
@@ -5,6 +5,9 @@ class StickyNotes(BotPlugin):
     def setup(self, *args, **kwargs):
         self.notes = self.open_storage('sticky_notes')
 
+    def sender(self, x, event):
+        return lambda: self.msg(x, event=event)
+
     @cmd
     def stickynote(self, event):
         """Create a sticky note for a given user."""
@@ -32,9 +35,6 @@ class StickyNotes(BotPlugin):
     def send_notes(self, event):
         format_note = lambda x, y: "{0}: {1}".format(x, y)
 
-        def sender(x):
-            return lambda: self.msg(x, event=event)
-
         if event.event_type == 'join':
             nick = event.meta['nick']
             if nick in self.notes and len(self.notes[nick]) > 0:
@@ -43,6 +43,27 @@ class StickyNotes(BotPlugin):
                 while len(notes) >= 1:
                     note = notes.pop()
                     self.delay_task(len(notes),
-                                    sender(format_note(nick, note)))
+                                    self.sender(format_note(nick, note), event))
                 self.notes[nick] = []
                 return format_note(nick, last_note)
+
+    @cmd
+    def list_stickynotes(self, event):
+        """
+        Lists users with pending sticky notes if no argument is given, sticky
+        notes for given user otherwise.
+        """
+        args = event.args
+        format_note = lambda x, y: "{0}. {1}".format(x, y)
+
+        if len(args) == 0:
+            return 'I have sticky notes for: ' + ', '.join(self.notes.keys())
+
+        if len(args) == 1:
+            nick = args[0]
+            user_notes = self.notes[nick]
+            first_note = format_note(1, user_notes.pop(0))
+            for index, note in enumerate(user_notes, start=2):
+                self.delay_task(index,
+                                self.sender(format_note(index, note), event))
+            return first_note
